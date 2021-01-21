e45e31e0d36360dd1471dfa13ccd7369b189edd6
rizalrahardi <rizalrahardi@gmail.com>
2021-01-21 21:26:26 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function hitungLuas() {
+    let jariJari = prompt("Masukan Nilai Jari-Jari", );
+    console.log("Nilai Jari-Jari = ", jariJari);
+    let hasil = prompt("Hasil Perhitungan 3.14xr^2",3.14*Math.pow(jariJari,2) );
+    console.log("Luas Lingkaran =", hasil);
+}
